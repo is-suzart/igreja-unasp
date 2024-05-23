19661a109ba81739778a2451b279c94418b60f86
igorarsuzart <igorarsuzart@gmail.com>
2024-05-23 23:56:55 +0000

    Mais uma coisinha

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,24 +7,28 @@ export default async function Blog(){
             <div className="bg-slate-900 flex items-center justify-center h-64 pt-10">
                 <h1 className="font-black text-4xl text-center text-white">Blog</h1>
             </div>
-            {
-                blogs.map((blog:BlogModel) => {
-                    const divStyle = {
-                        backgroundImage: `url(${blog.thumb})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    };
-                    return(
-                        
-                        <div>
-                            <div className="w-full h-24" style={divStyle}></div>
-                            <h3 className="font-bold text-base text-slate-900">{blog.title}</h3>
-                            <p className="text-sm text-slate-500">{blog.seo.description}</p>
-                        </div>
-                        
-                    )
-                })
-            }
+            <div className="grid gap-8 grid-cols-3 mt-8 px-8">
+                {
+                    blogs.map((blog:BlogModel) => {
+                        const divStyle = {
+                            backgroundImage: `url(${blog.thumb})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        };
+                        return(
+                            
+                            <div className="bg-white rounded-lg shadow-lg">
+                                <div className="w-full rounded-t-lg h-48" style={divStyle}></div>
+                                <div className="p-4">
+                                    <h3 className="font-bold text-base text-slate-700">{blog.title}</h3>
+                                    <p className="text-sm mt-1 text-slate-500 line-clamp-3">{blog.seo.description}</p>
+                                </div>
+                            </div>
+                            
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
