def97aa10340f4f715ae2664f9f174ddd740139a
Igor Suzart <igorarsuzart@gmail.com>
2024-07-16 13:12:21 -0300

    Não ta 100%

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import Button from "../components/button";
 import { ButtonModel } from "../models/layout";
 import { getLinks } from "../services/home";
 
 export default async function Links(){
     const links = await getLinks('links');
-    const bgStyle = {
+    const bgStyle = await {
         backgroundImage: `url(${links.background})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
@@ -12,7 +13,9 @@ export default async function Links(){
     return (
         <main style={bgStyle} className="w-full h-screen px-8 flex items-center flex-col">
             <div className="py-8 flex justify-center items-center">
-                <img src="https://igrejaunasp.com/api/wp-content/uploads/2024/04/logo-unasp-white.svg" alt="Logo igreja Unasp" />
+                <a href={'/'}>
+                    <img src="https://igrejaunasp.com/api/wp-content/uploads/2024/04/logo-unasp-white.svg" alt="Logo igreja Unasp" />
+                </a>
             </div>
             <div className="py-12 xl:py-24 2xl:py-32 w-full md:w-3/4 xl:w-2/4 2xl:w-2/3">
             {
