cb42e497b9ed56f6a2edd1fc31280d50393bb3d1
Igor Suzart <igorarsuzart@gmail.com>
2024-10-07 14:29:34 -0300

    Ainda falta umas tags seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,27 @@ import { getPage } from "./_services/home";
 import { Section } from "./_components/section";
 import { SectionModel } from "./_models/page.model";
 import { Anuncio } from "./_components/ad";
+import { Metadata } from "next";
 
+const homePage = await getPage('igreja-unasp');
+
+export const metadata:Metadata = {
+    title: homePage.seo.title,
+    description: homePage.seo.description,
+    robots: homePage.seo.robots,
+    icons: {
+      icon: "https://igrejaunasp.com/api/wp-content/uploads/2024/08/icone-unasp.png"
+    },
+    openGraph: {
+      title: homePage.seo.og_title,
+      description: homePage.seo.og_description,
+      url: "https://igrejaunasp.com",
+      siteName: homePage.seo.og_site_name,
+    }
+
+}
 
 export default async function Home() {
-  const homePage = await getPage('igreja-unasp');
   return (
     <main className="">
       <Banner bannerData={homePage.banner} />
--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,11 +1,30 @@
+import { Metadata } from "next";
 import { Anuncio } from "../_components/ad";
 import { Banner } from "../_components/banner";
 import { Section } from "../_components/section";
 import { SectionModel } from "../_models/page.model";
 import { getPage } from "../_services/home";
 
+const sobrePage = await getPage('sobre');
+
+export const metadata:Metadata = {
+    title: sobrePage.seo.title,
+    description: sobrePage.seo.description,
+    robots: sobrePage.seo.robots,
+    icons: {
+      icon: "https://igrejaunasp.com/api/wp-content/uploads/2024/08/icone-unasp.png"
+    },
+    openGraph: {
+      title: sobrePage.seo.og_title,
+      description: sobrePage.seo.og_description,
+      url: "https://igrejaunasp.com",
+      siteName: sobrePage.seo.og_site_name,
+    }
+
+}
+
 export default async function Sobre(){
-    const sobrePage = await getPage('sobre');
+    
     return (
         <main>
             <Banner bannerData={sobrePage.banner} />
